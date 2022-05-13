8d78252c8f8211e228a9c2ea060fbedd728bb9b0
VictorTao1998 <jit124@ucsd.edu>
2022-05-12 19:25:04 -0700

    config

--- api/utils.py
+++ api/utils.py
@@ -4,107 +4,12 @@ import struct
 
 import numpy as np
 import cv2
-import Imath
-import OpenEXR
+
 from PIL import Image
 # import torch
 # from torchvision.utils import make_grid
 
 
-def exr_loader(EXR_PATH, ndim=3):
-    """Loads a .exr file as a numpy array
-
-    Args:
-        EXR_PATH: path to the exr file
-        ndim: number of channels that should be in returned array. Valid values are 1 and 3.
-                        if ndim=1, only the 'R' channel is taken from exr file
-                        if ndim=3, the 'R', 'G' and 'B' channels are taken from exr file.
-                            The exr file must have 3 channels in this case.
-    Returns:
-        numpy.ndarray (dtype=np.float32): If ndim=1, shape is (height x width)
-                                          If ndim=3, shape is (3 x height x width)
-
-    """
-
-    exr_file = OpenEXR.InputFile(EXR_PATH)
-    cm_dw = exr_file.header()['dataWindow']
-    size = (cm_dw.max.x - cm_dw.min.x + 1, cm_dw.max.y - cm_dw.min.y + 1)
-
-    pt = Imath.PixelType(Imath.PixelType.FLOAT)
-
-    if ndim == 3:
-        # read channels indivudally
-        allchannels = []
-        for c in ['R', 'G', 'B']:
-            # transform data to numpy
-            channel = np.frombuffer(exr_file.channel(c, pt), dtype=np.float32)
-            channel.shape = (size[1], size[0])
-            allchannels.append(channel)
-
-        # create array and transpose dimensions to match tensor style
-        exr_arr = np.array(allchannels).transpose((0, 1, 2))
-        return exr_arr
-
-    if ndim == 1:
-        # transform data to numpy
-        channel = np.frombuffer(exr_file.channel('R', pt), dtype=np.float32)
-        channel.shape = (size[1], size[0])  # Numpy arrays are (row, col)
-        exr_arr = np.array(channel)
-        return exr_arr
-
-
-def exr_saver(EXR_PATH, ndarr, ndim=3):
-    '''Saves a numpy array as an EXR file with HALF precision (float16)
-    Args:
-        EXR_PATH (str): The path to which file will be saved
-        ndarr (ndarray): A numpy array containing img data
-        ndim (int): The num of dimensions in the saved exr image, either 3 or 1.
-                        If ndim = 3, ndarr should be of shape (height, width) or (3 x height x width),
-                        If ndim = 1, ndarr should be of shape (height, width)
-    Returns:
-        None
-    '''
-    if ndim == 3:
-        # Check params
-        if len(ndarr.shape) == 2:
-            # If a depth image of shape (height x width) is passed, convert into shape (3 x height x width)
-            ndarr = np.stack((ndarr, ndarr, ndarr), axis=0)
-
-        if ndarr.shape[0] != 3 or len(ndarr.shape) != 3:
-            raise ValueError(
-                'The shape of the tensor should be (3 x height x width) for ndim = 3. Given shape is {}'.format(
-                    ndarr.shape))
-
-        # Convert each channel to strings
-        Rs = ndarr[0, :, :].astype(np.float16).tostring()
-        Gs = ndarr[1, :, :].astype(np.float16).tostring()
-        Bs = ndarr[2, :, :].astype(np.float16).tostring()
-
-        # Write the three color channels to the output file
-        HEADER = OpenEXR.Header(ndarr.shape[2], ndarr.shape[1])
-        half_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.HALF))
-        HEADER['channels'] = dict([(c, half_chan) for c in "RGB"])
-
-        out = OpenEXR.OutputFile(EXR_PATH, HEADER)
-        out.writePixels({'R': Rs, 'G': Gs, 'B': Bs})
-        out.close()
-    elif ndim == 1:
-        # Check params
-        if len(ndarr.shape) != 2:
-            raise ValueError(('The shape of the tensor should be (height x width) for ndim = 1. ' +
-                              'Given shape is {}'.format(ndarr.shape)))
-
-        # Convert each channel to strings
-        Rs = ndarr[:, :].astype(np.float16).tostring()
-
-        # Write the color channel to the output file
-        HEADER = OpenEXR.Header(ndarr.shape[1], ndarr.shape[0])
-        half_chan = Imath.Channel(Imath.PixelType(Imath.PixelType.HALF))
-        HEADER['channels'] = dict([(c, half_chan) for c in "R"])
-
-        out = OpenEXR.OutputFile(EXR_PATH, HEADER)
-        out.writePixels({'R': Rs})
-        out.close()
 
 
 def save_uint16_png(path, image):
